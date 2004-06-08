5f4bce4bad4a60e52756bc26a3171a48069083a4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2004-06-08 08:21:30 +0000

    *** empty log message ***

---
+++ p4a/i18n/messages/de/DE.php
@@ -0,0 +1,90 @@
+<?
+
+/**
+ * P4A - PHP For Applications.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * To contact the authors write to:									<br>
+ * CreaLabs															<br>
+ * Viale dei Mughetti 13/A											<br>
+ * 10151 Torino (Italy)												<br>
+ * Tel.:   (+39) 011 735645											<br>
+ * Fax:    (+39) 011 735645											<br>
+ * Web:    {@link http://www.crealabs.it}							<br>
+ * E-mail: {@link mailto:info@crealabs.it info@crealabs.it}
+ *
+ * The latest version of p4a can be obtained from:
+ * {@link http://p4a.sourceforge.net}
+ *
+ * @link http://p4a.sourceforge.net
+ * @link http://www.crealabs.it
+ * @link mailto:info@crealabs.it info@crealabs.it
+ * @copyright CreaLabs
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
+ * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @package p4a
+ */
+
+$messages = array
+(
+	// Date
+    "days"				=> array('Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'),
+    "months"			=> array('Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'),
+    
+	// Standard Toolbar
+	"big_confirm"		=> "Änderungen übernehmen.",
+	"big_cancel"		=> "Operation abbrechen.",
+	"big_new"			=> "neuen Datensatz einfügen.",
+	"big_delete"		=> "Datensatz löschen.",
+	"big_first"			=> "zum Ersten.",
+	"big_prev"			=> "zum Vorhergehenden.",
+	"big_next"			=> "zum Nächsten.",
+	"big_last"			=> "zum Letzten.",
+	"big_print"			=> "drucken.",
+	"big_exit"			=> "beenden.",
+	"little_first"		=> "zum Ersten.",
+	"little_prev"		=> "zum Vorhergehenden.",
+	"little_next"		=> "zum Nächsten.",
+	"little_last"		=> "zum Letzten.",
+	"go"				=> "los",
+	
+	// Table
+	"current_page"		=> "Seite",
+	"of_pages"			=> "von",
+	"go_to_page"		=> "gehe zu Seite",
+	
+	// File uploads
+	"filename"			=> "Name",
+	"filesize"			=> "Größe",
+	"filetype"			=> "Typ",
+	"filepreview"		=> "Vorschau",
+	"filedelete"		=> "löschen",
+	
+	// Fields
+	"none_selected"		=> "Felder",
+	
+	// Confirmations
+	"confirm_general"	=> "Sind Sie sich sicher?",
+	"confirm_confirm"	=> "Änderungen übernehmen?",
+	"confirm_cancel"	=> "Laufende Operation wirklich abbrechen?",
+	"confirm_delete"	=> "Diesen Datensatz wirklich löschen?",
+	
+	// Yes/No
+	"yes"				=> "Ja",
+	"no"				=> "Nein"
+);
+
+?>
