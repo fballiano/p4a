780767a0e4827e6b973afdb593520a259ef3a973
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2004-06-11 13:11:26 +0000

    *** empty log message ***

---
+++ p4a/i18n/messages/pt/BR.php
@@ -0,0 +1,90 @@
+<?
+
+/**
+ * P4A - PHP For Applications.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * To contact the authors write to:									<br>
+ * CreaLabs															<br>
+ * Viale dei Mughetti 13/A											<br>
+ * 10151 Torino (Italy)												<br>
+ * Tel.:   (+39) 011 735645											<br>
+ * Fax:    (+39) 011 735645											<br>
+ * Web:    {@link http://www.crealabs.it}							<br>
+ * E-mail: {@link mailto:info@crealabs.it info@crealabs.it}
+ *
+ * The latest version of p4a can be obtained from:
+ * {@link http://p4a.sourceforge.net}
+ *
+ * @link http://p4a.sourceforge.net
+ * @link http://www.crealabs.it
+ * @link mailto:info@crealabs.it info@crealabs.it
+ * @copyright CreaLabs
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
+ * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @package p4a
+ */
+
+$messages = array
+(
+	// Date
+    "days"				=> array('Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'),
+    "months"			=> array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'),
+    
+	// Standard Toolbar
+	"big_confirm"		=> "Confirmar e salvar.",
+	"big_cancel"		=> "Cancelar operação atual.",
+	"big_new"			=> "Inserir novo registro.",
+	"big_delete"		=> "Apagar registro atual.",
+	"big_first"			=> "Ir para o primeiro.",
+	"big_prev"			=> "Ir para o anterior.",
+	"big_next"			=> "Ir para o próximo.",
+	"big_last"			=> "Ir para o último.",
+	"big_print"			=> "Imprimir.",
+	"big_exit"			=> "Sair.",
+	"little_first"		=> "Ir para o primeiro.",
+	"little_prev"		=> "Ir para o anterior.",
+	"little_next"		=> "Ir para o próximo.",
+	"little_last"		=> "Ir para o último.",
+	"go"				=> "Ir",
+	
+	// Table
+	"current_page"		=> "Página",
+	"of_pages"			=> "de",
+	"go_to_page"		=> "Ir para página",
+	
+	// File uploads
+	"filename"			=> "Nome",
+	"filesize"			=> "Tamanho",
+	"filetype"			=> "Tipo",
+	"filepreview"		=> "Pré-visualizar",
+	"filedelete"		=> "Apagar",
+	
+	// Fields
+	"none_selected"		=> "Não Selecionado",
+	
+	// Confirmations
+	"confirm_general"	=> "Você tem certeza?",
+	"confirm_confirm"	=> "Confirmar e salvar?",
+	"confirm_cancel"	=> "Cancelar operação atual?",
+	"confirm_delete"	=> "Apagar registro atual?",
+	
+	// Yes/No
+	"yes"				=> "Sim",
+	"no"				=> "Não"
+);
+
+?>
