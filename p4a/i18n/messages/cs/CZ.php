55e1c39694a2936a8e58778b499cf2346de4a1de
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2004-07-26 13:06:04 +0000

    *** empty log message ***

---
+++ p4a/i18n/messages/cs/CZ.php
@@ -0,0 +1,90 @@
+<?
+
+/**
+ * MerlinWork: Merlin Web Application Framework.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * To contact the authors write to:									<br>
+ * CreaLabs															<br>
+ * Viale dei Mughetti 13/A											<br>
+ * 10151 Torino (Italy)												<br>
+ * Tel.:   (+39) 011 735645											<br>
+ * Fax:    (+39) 011 735645											<br>
+ * Web:    {@link http://www.crealabs.it}							<br>
+ * E-mail: {@link mailto:info@crealabs.it info@crealabs.it}
+ *
+ * The latest version of MerlinWork can be obtained from:
+ * {@link http://merlinwork.sourceforge.net}
+ *
+ * @link http://merlinwork.sourceforge.net
+ * @link http://www.crealabs.it
+ * @link mailto:info@crealabs.it info@crealabs.it
+ * @copyright CreaLabs
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
+ * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @package MerlinWork
+ */
+
+$messages = array
+(
+	// Date
+    "days"				=> array('Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota'),
+    "months"			=> array('Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'),
+    
+	// Standard Toolbar
+	"big_confirm"		=> "Potvrdit a uložit.",
+	"big_cancel"		=> "Zrušit tuto operaci.",
+	"big_new"			=> "Přidat nový záznam.",
+	"big_delete"		=> "Vymazat tento záznam.",
+	"big_first"			=> "Jdi na první.",
+	"big_prev"			=> "Jdi na předchozí.",
+	"big_next"			=> "Jdi na další.",
+	"big_last"			=> "Jdi na poslední.",
+	"big_print"			=> "Tisk.",
+	"big_exit"			=> "Odchod.",
+	"little_first"		=> "Jdi na první.",
+	"little_prev"		=> "Jdi na předchozí.",
+	"little_next"		=> "Jdi na další.",
+	"little_last"		=> "Jdi na poslední.",
+	"go"				=> "Jdi",
+	
+	// Table
+	"current_page"		=> "Strana",
+	"of_pages"			=> "z",
+	"go_to_page"		=> "Jdi na stranu",
+	
+	// File uploads
+	"filename"			=> "Jméno",
+	"filesize"			=> "Velikost",
+	"filetype"			=> "Typ",
+	"filepreview"		=> "Náhled",
+	"filedelete"		=> "Vymazat",
+	
+	// Fields
+	"none_selected"		=> "Není vybráno",
+	
+	// Confirmations
+	"confirm_general"	=> "Jste si jist?",
+	"confirm_confirm"	=> "Potvrdit a uložit?",
+	"confirm_cancel"	=> "Zrušit tuto operaci?",
+	"confirm_delete"	=> "Vymazat tento záznam?",
+	
+	// Yes/No
+	"yes"				=> "Ano",
+	"no"				=> "Ne"
+);
+
+?>
