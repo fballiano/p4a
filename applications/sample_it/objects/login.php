5d1d4e1c3c28dd993d146ca96f6209845f36737f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2004-11-11 08:40:23 +0000

    *** empty log message ***

---
+++ applications/sample_it/objects/login.php
@@ -0,0 +1,11 @@
+<?php
+
+class Login extends P4A_Mask
+{
+	function &Login()
+	{
+		parent::P4A_Mask();
+	}
+}
+
+?>
--- applications/sample_it/objects/sample_it.php
+++ applications/sample_it/objects/sample_it.php
@@ -1,56 +1,66 @@
 <?php
+
 class Sample_it extends P4A
 {
 	function &Sample_It()
 	{
-// 		Prima di tutto richiamo il costruttore di p4a 		
+		//Prima di tutto richiamo il costruttore di p4a
 		parent::p4a();
-		
-		/*Per istanziare un oggetto e' necessario usare il metodo
-		build. Qualsiasi oggetto e' in grado di costruire un 
+
+		/*
+		Per istanziare un oggetto e' necessario usare il metodo
+		build. Qualsiasi oggetto e' in grado di costruire un
 		qualsiasi altro oggetto "figlio" attraverso il metodo build
-		che prende 2 parametri, il tipo di oggetto (p4a_menu)e il nome 
+		che prende 2 parametri, il tipo di oggetto (p4a_menu)e il nome
 		che ho deciso di assegnare all'oggetto( menu_master ).
-		Il "figlio" si posiziona in una variabile che si chiama 
+		Il "figlio" si posiziona in una variabile che si chiama
 		come il nome (menu_master) all'interno del parent;
-		nell'esempio potrei quindi accedere al menu all'interno 
+		nell'esempio potrei quindi accedere al menu all'interno
 		della classe corrente digitando $this->menu_master o dovunque
 		nel progetto cosi':
 		$p4a =& sample_it::singleton();
 		$p4a->menu_master....
-		Il metodo build restituisce anche l'oggetto appena creato.*/
+		Il metodo build restituisce anche l'oggetto appena creato.
+		*/
 		$menu =& $this->build("p4a_menu", "menu_master");
-		
-		/* Il metodo additem del menu permette di aggiungere 
+
+		/*
+		Il metodo additem del menu permette di aggiungere
 		voci al menu stesso. Una voce aggiunta al menu la ritrovo
 		all'interno della collection items del menu:
 		$menu->items->utenti.
 		Volessi creare sottovoci di utenti potrei farlo cosi':
 		$menu->items->utenti->addItem("sottovoce") e quindi accedervi
-		cosi': $menu->items->utenti->items->sottovoce*/
+		cosi': $menu->items->utenti->items->sottovoce
+		*/
 		$menu->addItem("utenti");
-		
-		/*Una delle funzionalità principali di p4a e' la 
+
+		/*
+		Una delle funzionalità principali di p4a e' la
 		possibilità di intercettare gli eventi. Qualsiasi oggetto
-		di p4a e' in grado di intercettare un evento attraverso 
+		di p4a e' in grado di intercettare un evento attraverso
 		il metodo intercept che prende tre parametri:
-		l'oggetto che causa l'evento, l'envento che si intende 
+		l'oggetto che causa l'evento, l'envento che si intende
 		intercettare e il metodo che deve venir richiamato al
 		generarsi dell'evento;
 		nell'esempio l'applicazione sta intercettando l'evento
-		onClick della voce utenti del menu attraverso la 
-		funzione utenti_click*/
+		onClick della voce utenti del menu attraverso la
+		funzione utenti_click
+		*/
 		$this->intercept($menu->items->utenti, "onClick", "utenti_click");
-		
-		/*openMask e' un metodo di p4a. La maschera e' un oggetto che 
+
+		/*
+		openMask e' un metodo di p4a. La maschera e' un oggetto che
 		deve essere prensente dentro la dir objects dell'applicazione
-		corrente e deve estendere la classe p4a_mask*/
-		$this->openMask("login"); 		
+		corrente e deve estendere la classe p4a_mask
+		*/
+		$this->openMask("login");
 	}
-	
+
 	function utenti_click()
 	{
 		$this->openMask("utenti");
 	}
 }
+
 ?>
