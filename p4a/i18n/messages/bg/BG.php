04a5ac342fbf9333d3f529747e7c7c67b23c8999
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2006-10-21 15:12:59 +0000

    *** empty log message ***

---
+++ p4a/i18n/messages/bg/BG.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * P4A - PHP For Applications.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * To contact the authors write to:									<br>
+ * CreaLabs															<br>
+ * Via Medail, 32													<br>
+ * 10144 Torino (Italy)												<br>
+ * Web:    {@link http://www.crealabs.it}							<br>
+ * E-mail: {@link mailto:info@crealabs.it info@crealabs.it}
+ *
+ * The latest version of p4a can be obtained from:
+ * {@link http://p4a.sourceforge.net}
+ *
+ * @link http://p4a.sourceforge.net
+ * @link http://www.crealabs.it
+ * @link mailto:info@crealabs.it info@crealabs.it
+ * @copyright CreaLabs
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
+ * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @package p4a
+ */
+
+$messages = array
+(
+	// Date
+    "days"		=> array('Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'),
+    "months"	=> array('Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'),
+
+	// Standard Toolbar
+	"save"				=> "Потвърди запаметяване",
+	"cancel"			=> "Прекъсни текущата операция",
+	"new"				=> "Въведи нов запис",
+	"delete"			=> "Изтрий текущия запис",
+	"print"				=> "Разпечатай",
+	"exit"				=> "Изход",
+	"first"				=> "Отиди до началния елемент",
+	"prev"				=> "Отиди до предишния елемент",
+	"next"				=> "Отиди до следващия елемент",
+	"last"				=> "Отиди до последния елемент",
+	"first_page"		=> "Отиди на първата страница",
+	"prev_page"			=> "Отиди на предишната страница",
+	"next_page"			=> "Отиди на следващата страница",
+	"last_page"			=> "Отиди на последната страница",
+	"go"				=> "Продължи",
+
+	// Table
+	"current_page"		=> "Страница",
+	"of_pages"			=> "от",
+	"go_to_page"		=> "Отиди на страница",
+	"ascending"			=> "Възходящо",
+	"descending"		=> "Низходящо",
+	"selected"			=> "Избрано",
+
+	// File uploads
+	"filename"			=> "Наименование",
+	"filesize"			=> "Размер",
+	"filetype"			=> "Тип",
+	"filepreview"		=> "Преглед",
+	"filedelete"		=> "Изтриване",
+	"filedownload"		=> "Изтегляне",
+
+	// Fields
+	"none_selected"		=> "Нищо избрано",
+
+	// Confirmations
+	"confirm_general"	=> "Сигурни ли сте?",
+	"confirm_confirm"	=> "Потвърждавате ли запис?",
+	"confirm_cancel"	=> "Да прекъсне ли текущата операция?",
+	"confirm_delete"	=> "Да бъде ли изтрит текущия запис?",
+
+	// Yes/No
+	"yes"				=> "Да",
+	"no"				=> "Не"
+);
+
+?>
