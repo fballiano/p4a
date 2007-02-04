41056484ca21a969aac194f8593af59fdc56048f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2007-02-04 16:25:06 +0000

    sample_i18n, some messages were changed

--- applications/sample_i18n/i18n/en/US.php
+++ applications/sample_i18n/i18n/en/US.php
@@ -1,4 +1,4 @@
 <?php
 
-$messages['hello_world'] = 'Welcome, this is a message';
+$messages['hello_world'] = 'Welcome, this is a message in english, open the source code to understand how to change locale and how the localization system works.';
 $messages['field_1'] = 'A test field';
--- applications/sample_i18n/i18n/it/IT.php
+++ applications/sample_i18n/i18n/it/IT.php
@@ -1,4 +1,4 @@
 <?php
 
-$messages['hello_world'] = 'Benvenuto, questo è un messaggio';
+$messages['hello_world'] = 'Benvenuto, questo è un messaggio in italiano, apri il codice sorgente dell\'applicazione per capire come cambiare locale e come funziona il sistema di localizzazione.';
 $messages['field_1'] = 'Campo di prova';
