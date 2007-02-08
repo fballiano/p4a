37ab8054a97e6860d486ff61d8fae07b5ce23618
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2007-02-08 09:29:35 +0000

    - Croatian traslation was added (thanks to Nenad Trupka) - some fixes were applied to Croatian formats (thanks to Nenad Trupka)

---
+++ p4a/i18n/messages/hr/HR.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * P4A - PHP For Applications.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * To contact the authors write to:									<br>
+ * CreaLabs															<br>
+ * Via Medail, 32													<br>
+ * 10144 Torino (Italy)												<br>
+ * Web:    {@link http://www.crealabs.it}							<br>
+ * E-mail: {@link mailto:info@crealabs.it info@crealabs.it}
+ *
+ * The latest version of p4a can be obtained from:
+ * {@link http://p4a.sourceforge.net}
+ *
+ * @link http://p4a.sourceforge.net
+ * @link http://www.crealabs.it
+ * @link mailto:info@crealabs.it info@crealabs.it
+ * @copyright CreaLabs
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
+ * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @package p4a
+ */
+
+$messages = array
+(
+	// Date
+    "days"				=> array('nedjelja' , 'ponedjeljak' , 'utorak' , 'srijeda' , 'četvrtak' , 'petak' , 'subota'),
+	"months"			=> array('siječanj', 'veljača', 'ožujak', 'travanj', 'svibanj', 'lipanj', 'srpanj', 'kolovoz', 'rujan', 'listopad', 'studeni', 'prosinac'),
+
+	// Standard Toolbar
+	"save"				=> "Potvrdi i snimi",
+	"cancel"			=> "Poništi tekuću operaciju",
+	"new"				=> "Dodaj novi zapis",
+	"delete"			=> "Obriši tekući zapis",
+	"print"				=> "Ispiši",
+	"exit"				=> "Izlaz",
+	"first"				=> "Idi na prvi element",
+	"prev"				=> "Idi na prethodni element",
+	"next"				=> "Idi na slijedeći element",
+	"last"				=> "Idi na posljednji element",
+	"first_page"		=> "Idi na prvu stranicu",
+	"prev_page"			=> "Idi na prethodnu stranicu",
+	"next_page"			=> "Idi na slijedeću stranicu",
+	"last_page"			=> "Idi na posljednju stranicu",
+	"go"				=> "Idi",
+
+	// Table
+	"current_page"		=> "Strana",
+	"of_pages"			=> "od",
+	"go_to_page"		=> "Idi na stranu",
+	"ascending"			=> "Rastući",
+	"descending"		=> "Padajući",
+	"selected"			=> "Odabrano",
+
+	// File uploads
+	"filename"			=> "Naziv",
+	"filesize"			=> "Veličina",
+	"filetype"			=> "Tip",
+	"filepreview"		=> "Pregled",
+	"filedelete"		=> "Obriši",
+	"filedownload"		=> "Download",
+
+	// Fields
+	"none_selected"		=> "Ništa nije odabrano",
+
+	// Confirmations
+	"confirm_general"	=> "Jeste li sigurni?",
+	"confirm_confirm"	=> "Potvrdi i snimi?",
+	"confirm_cancel"	=> "Odustani od operacije?",
+	"confirm_delete"	=> "Obriši zapis?",
+
+	// Yes/No
+	"yes"				=> "Da",
+	"no"				=> "Ne"
+);
