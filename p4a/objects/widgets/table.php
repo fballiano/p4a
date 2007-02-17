70f50ea89b3970c6d288ab97428fa4daaabe397d
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2007-02-17 00:53:35 +0000

    a bug with P4A_Table and long numbers formatting was fixed

--- p4a/objects/widgets/table.php
+++ p4a/objects/widgets/table.php
@@ -959,9 +959,9 @@
 						}
 					} elseif ($parent->cols->$col_name->isFormatted()) {
 						if (($parent->cols->$col_name->formatter_name === null) and ($parent->cols->$col_name->format_name === null)) {
-							$aReturn[$i]['cells'][$j]['value'] = $p4a->i18n->autoFormat($row[$col_name], $parent->data->fields->$col_name->getType());
+							$aReturn[$i]['cells'][$j]['value'] = str_replace(' ', '&nbsp;', $p4a->i18n->autoFormat($row[$col_name], $parent->data->fields->$col_name->getType()));
 						} else {
-							$aReturn[$i]['cells'][$j]['value'] = $p4a->i18n->{$parent->cols->$col_name->formatter_name}->format( $row[$col_name], $p4a->i18n->{$parent->cols->$col_name->formatter_name}->getFormat($parent->cols->$col_name->format_name));
+							$aReturn[$i]['cells'][$j]['value'] = str_replace(' ', '&nbsp;', $p4a->i18n->{$parent->cols->$col_name->formatter_name}->format( $row[$col_name], $p4a->i18n->{$parent->cols->$col_name->formatter_name}->getFormat($parent->cols->$col_name->format_name)));
 						}
 					} else {
 						$aReturn[$i]['cells'][$j]['value'] = $row[$col_name];
