223a91ed97f977f5e1b545482cedc71130a2c761
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2010-03-22 09:03:07 +0000

    es_MX translation was added to products_catalogue sample application

---
+++ applications/products_catalogue/i18n/es_MX.php
@@ -0,0 +1,70 @@
+﻿<?php
+/**
+ * This file is part of P4A - PHP For Applications.
+ *
+ * P4A is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * P4A is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with P4A.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ * To contact the authors write to:									<br />
+ * CreaLabs SNC														<br />
+ * Via Medail, 32													<br />
+ * 10144 Torino (Italy)												<br />
+ * Website: {@link http://www.crealabs.it}							<br />
+ * E-mail: {@link mailto:info@crealabs.it info@crealabs.it}
+ *
+ * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
+ * @copyright CreaLabs SNC
+ * @link http://www.crealabs.it
+ * @link http://p4a.sourceforge.net
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @package p4a
+ */
+
+$msg = array(
+	// general
+	"Visible" => "Visible",
+	"Description" => "Descripción",
+
+	// menu
+	"Products" => "Productos",
+	"Support Tables" => "Tablas de Soporte",
+	"Categories" => "Categorías",
+	"Brands" => "Marcas",
+	"Change language" => "Cambiar idioma",
+
+	// products mask
+	"Search" => "Buscar",
+	"Model" => "Modelo",
+	"Date arrival" => "Fecha de ingreso",
+	"Product detail" => "Detalles del producto",
+	"Product ID" => "ID del Producto",
+	"Category" => "Categoría",
+	"Brand" => "Marca",
+	"Price $" => "Precio $",
+	"Discount %" => "Descuento %",
+	"Picture" => "Imagen",
+	"Is new" => "Es nuevo?",
+
+	// categories mask
+	"Category ID" => "ID de la Categoría",
+	"Category detail" => "Detalles de la Categoria",
+
+	// brands mask
+	"Brand detail" => "Detalles de la Marca",
+	"Brand ID" => "ID de la Marca",
+	
+	// change language mask
+	"Choose language" => "Seleccionar idioma",
+	"Apply" => "Aplicar"
+);
